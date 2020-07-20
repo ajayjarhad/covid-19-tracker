1be6e136e11f48b61e6847dc3ef69716a83cfd22
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-07-21 01:28:27 +0530

    Project done,refactoring - pending

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ state = {
     data : {},
     country:''
 }
-
-    handleCountryChange = async(country)=>{
-        console.log(country)
-         
-    }
-
     async componentDidMount() {
         const fetchedData = await fetchData()
 
         this.setState({data:fetchedData})
     }
+    handleCountryChange = async(country)=>{
+        const fetchedData = await fetchData(country)
+        
+        this.setState({ data:fetchedData,country:country })
+         
+    }
 
     render(){
-    const {data} = this.state
+    const {data,country} = this.state
 
         return(
             <div className={styles.container}>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country}/>
             </div>
         )
     }
